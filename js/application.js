fd61850a4f0cd16c16a66e07762afacdc4a36edc
Andrew Landry <andrewlandry@gmail.com>
2013-08-22 21:45:49 -0400

    add timer
    add coffee timer that takes user input in seconds

--- js/application.js
+++ js/application.js
@@ -1,25 +1,46 @@
 $(document).ready(function() {
-  $('#water').on('keyup', function() {
+  $water = $('#water');
+  $coffee = $('#coffee');
+  $totalCoffee = $('#totalCoffee');
+
+  $water.on('keyup', function() {
     var amountWater = +$(this).val();
     $('#totalWater').text((amountWater / 17).toFixed(2));
   });
-  $('#coffee').on('keyup', function() {
+  $coffee.on('keyup', function() {
     var amountCoffee = +$(this).val();
-    $('#totalCoffee').text((amountCoffee * 17).toFixed(2));
+    $totalCoffee.text((amountCoffee * 17).toFixed(2));
   });
 
   $('#coffee_to_water').click(function() {
-    $('#coffee_to_water').toggleClass('highlighted');
+    $(this).toggleClass('highlighted');
     $('#water_to_coffee').toggleClass('highlighted');
     $('#coffee_form').toggleClass('hidden');
     $('#water_form').toggleClass('hidden');
   });
 
   $('#water_to_coffee').click(function() {
-    $('#water_to_coffee').toggleClass('highlighted');
+    $(this).toggleClass('highlighted');
     $('#coffee_to_water').toggleClass('highlighted');
     $('#coffee_form').toggleClass('hidden');
     $('#water_form').toggleClass('hidden');
   });
 
+  var countDown = function (time) {
+    $('#timer').val(time);
+    if (time < 1) {
+      alert('Your coffee is done!');
+      return;
+    }
+    setTimeout(function () {
+      countDown(time - 1);
+    }, 1000)
+  };
+
+  var startTimer = function() {
+    var time = parseInt($('#timer').val(), 10);
+    if (!(time > 0)) { time = 2; }
+    countDown(time);
+  }
+  $('#timer_button').click(startTimer)
 });
